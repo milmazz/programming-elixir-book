0352bb6a2185ea3e0a0a34820a695b4253fe5f34
Milton Mazzarri <milmazz@gmail.com>
2013-09-24 21:54:41 -0500

    Fixed the first approach

--- exercises/ModulesAndFunctions/6/chop.ex
+++ exercises/ModulesAndFunctions/6/chop.ex
@@ -1,16 +1,21 @@
 defmodule Chop do
 
-  def guess(actual, _min..max) when max == actual do
-  	IO.puts actual
+  def guess(actual, min..max) do
+    do_guess(actual, min..max, div(max, 2))
   end
 
-  def guess(actual, min..max) when max > actual do
-  	IO.puts "Is it #{max}"
-    guess(actual, min..min+div(max-min, 2))
+  defp do_guess(actual, min.._max, guess) when guess > actual do
+    IO.puts "Is it #{guess}"
+    do_guess(actual, min..guess - 1, min + div(guess - 1 - min, 2))
   end
 
-  def guess(actual, _min..max) when max < actual do
-    IO.puts "Is it #{max}"
-    guess(actual, max..max + div(max, 2))
+  defp do_guess(actual, _min..max, guess) when guess < actual do
+    IO.puts "Is it #{guess}"
+    do_guess(actual, guess + 1..max, guess + div(max - guess + 1, 2))
   end
+
+  defp do_guess(actual, _range, guess) when guess == actual do
+    IO.puts "Is it #{guess}\n#{guess}"
+  end
+
 end
