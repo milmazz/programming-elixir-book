f979f05d4d934247cefeb0e4a83a202869b1a1de
Milton Mazzarri <milmazz@gmail.com>
2013-09-28 21:31:50 -0500

    Fixed when found the maximum number in the list repeatedly

--- exercises/ListAndRecursion/2/max.exs
+++ exercises/ListAndRecursion/2/max.exs
@@ -2,7 +2,7 @@ defmodule MyList do
   def max([head | tail]), do: do_max(tail, head)
 
   defp maximum(a, b) when a > b, do: a
-  defp maximum(a, b) when a < b, do: b
+  defp maximum(a, b), do: b
 
   defp do_max([], value), do: value
   defp do_max([head | tail], value) do
