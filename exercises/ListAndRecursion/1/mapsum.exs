f29801aa63e48ca7d6dd82549fc6b434121700d3
Milton Mazzarri <milmazz@gmail.com>
2013-09-28 23:58:05 -0500

    mapsum function refactored

--- exercises/ListAndRecursion/1/mapsum.exs
+++ exercises/ListAndRecursion/1/mapsum.exs
@@ -1,9 +1,6 @@
 defmodule MyList do
-  def mapsun(list, func), do: do_sum(do_map(list, func))
-
-  defp do_map([], _func), do: []
-  defp do_map([head | tail], func), do: [func.(head) | do_map(tail, func)]
-
-  defp do_sum([]), do: 0
-  defp do_sum([head | tail]), do: head + do_sum(tail)
+  def mapsum([], _func), do: []
+  def mapsum([head | tail], func) do
+    func.(head) + mapsum(tail, func)
+  end
 end
